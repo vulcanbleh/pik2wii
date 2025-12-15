06edc7662d10c50e97e3569adb44f4df3fed9992
vulcanbleh <148649099+vulcanbleh@users.noreply.github.com>
2025-12-15 04:29:52 +0000

    further fix build

--- src/sysGCU/simpleMessage.cpp
+++ src/sysGCU/simpleMessage.cpp
@@ -30,7 +30,7 @@ void SimpleMessage::drawMessageID(Graphics& gfx, u32 lowerHalf, u32 upperHalf)
 	mProcessor->preProcID(lowerHalf, upperHalf);
 
 	JMessage::TRenderingProcessor* jmProc = static_cast<JMessage::TRenderingProcessor*>(mProcessor);
-	jmProc->reset_(nullptr);
+	//jmProc->reset_(nullptr);
 	jmProc->setBegin_messageID(lowerHalf, upperHalf, nullptr);
 	jmProc->process(nullptr);
 }
