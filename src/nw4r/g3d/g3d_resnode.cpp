13d0cda64d64fea7069e55c848fa1222c40549af
vulcanbleh <148649099+vulcanbleh@users.noreply.github.com>
2026-01-08 16:50:17 +0000

    link g3d_resnode.cpp

--- src/nw4r/g3d/g3d_resnode.cpp
+++ src/nw4r/g3d/g3d_resnode.cpp
@@ -20,31 +20,47 @@ void ResNode::PatchChrAnmResult(ChrAnmResult* pResult) const
 	if (flags & ChrAnmResult::FLAG_PATCH_SCALE) {
 		if (r.flags & ResNodeData::FLAG_SCALE_ONE) {
 			flags |= ChrAnmResult::FLAG_SCALE_ONE | ChrAnmResult::FLAG_SCALE_UNIFORM;
+			pResult->s.z = 1.f;
+            pResult->s.y = 1.f;
+            pResult->s.x = 1.f;
 		} else {
+			u32 flag_ = flags & ~ChrAnmResult::FLAG_SCALE_UNIFORM;
+			
 			if (r.flags & ResNodeData::FLAG_SCALE_UNIFORM) {
-				flags |= ChrAnmResult::FLAG_SCALE_UNIFORM;
+				flag_ = flags | ChrAnmResult::FLAG_SCALE_UNIFORM;
 			}
+			
+			flags = flag_ & ~(ChrAnmResult::FLAG_SCALE_ONE | ChrAnmResult::FLAG_MTX_IDENT);
 
 			pResult->s = static_cast<const math::VEC3&>(r.scale);
 		}
 	}
 
 	if (flags & ChrAnmResult::FLAG_PATCH_ROT) {
+		math::VEC3 trans(pResult->rt[0][3], pResult->rt[1][3], pResult->rt[2][3]);
 		if (r.flags & ResNodeData::FLAG_ROT_ZERO) {
-			flags |= ChrAnmResult::FLAG_ROT_ZERO;
+			math::MTX34Identity(&pResult->rt);
+            flags |= ChrAnmResult::FLAG_ROT_ZERO;
 		} else {
 			math::MTX34RotXYZDeg(&pResult->rt, r.rot.x, r.rot.y, r.rot.z);
-			flags &= ~ChrAnmResult::FLAG_ROT_ZERO;
+			flags &= ~(ChrAnmResult::FLAG_ROT_ZERO | ChrAnmResult::FLAG_ROT_TRANS_ZERO | ChrAnmResult::FLAG_MTX_IDENT);
 		}
 
 		flags |= ChrAnmResult::FLAG_ROT_RAW_FMT;
+		pResult->rt._03 = trans.x;
+        pResult->rt._13 = trans.y;
+        pResult->rt._23 = trans.z;
 	}
 
 	if (flags & ChrAnmResult::FLAG_PATCH_TRANS) {
 		if (r.flags & ResNodeData::FLAG_TRANS_ZERO) {
 			flags |= ChrAnmResult::FLAG_TRANS_ZERO;
+			
+			pResult->rt._23 = 0.f;
+            pResult->rt._13 = 0.f;
+            pResult->rt._03 = 0.f;
 		} else {
-			flags &= ~ChrAnmResult::FLAG_TRANS_ZERO;
+			flags &= ~(ChrAnmResult::FLAG_TRANS_ZERO | ChrAnmResult::FLAG_ROT_TRANS_ZERO | ChrAnmResult::FLAG_MTX_IDENT);
 
 			pResult->rt._03 = r.translate.x;
 			pResult->rt._13 = r.translate.y;
