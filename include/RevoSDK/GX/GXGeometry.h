990cbdfb0d54ab539ad59c53bd4473c6af44840a
vulcanbleh <148649099+vulcanbleh@users.noreply.github.com>
2026-02-05 06:00:18 +0000

    gx/vec tweaks

--- include/RevoSDK/GX/GXGeometry.h
+++ include/RevoSDK/GX/GXGeometry.h
@@ -23,7 +23,7 @@ extern void GXClearVtxDesc();
 extern void GXSetVtxAttrFmt(GXVtxFmt format, GXAttr attr, GXCompCnt count, GXCompType type, u8 frac);
 extern void GXSetVtxAttrFmtv(GXVtxFmt format, GXVtxAttrFmtList* list);
 
-extern void GXSetArray(GXAttr attr, void* basePtr, u8 stride);
+extern void GXSetArray(GXAttr attr, const void* basePtr, u8 stride);
 extern void GXInvalidateVtxCache();
 extern void GXSetTexCoordGen2(GXTexCoordID coord, GXTexGenType genType, GXTexGenSrc srcParam, u32 mtx, GXBool doNormalise, u32 postMtx);
 extern void GXSetNumTexGens(u8 count);
