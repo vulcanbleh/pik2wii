e7bb029f0ef5862e7069e8950a4f153940b82443
vulcanbleh <148649099+vulcanbleh@users.noreply.github.com>
2026-02-12 05:48:12 +0000

    eggAssert header needed now

---
+++ include/egg/prim/eggAssert.h
@@ -0,0 +1,41 @@
+#ifndef EGG_PRIM_ASSERT_H
+#define EGG_PRIM_ASSERT_H
+
+#include <egg/egg_types.h>
+
+#ifdef EGG_LOG
+#define EGG_PRINT(...) system_print(true, __FILE__, __VA_ARGS__)
+
+#define EGG_ASSERT(EXP)                                                        \
+    if (!(EXP))                                                                \
+    system_halt(__FILE__, __LINE__, #EXP)
+
+#define EGG_ASSERT_MSG(EXP, ...)                                               \
+    if (!(EXP))                                                                \
+    system_halt(__FILE__, __LINE__, __VA_ARGS__)
+#else
+#define EGG_PRINT(...) ((void)(0))
+#define EGG_ASSERT(EXP) ((void)(0))
+#define EGG_ASSERT_MSG(EXP, ...) ((void)(0))
+#endif
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#if defined(NONMATCHING) || defined(COMPAT_ANY)
+void system_print(bool visible, const char* pFile, const char* pMsg, ...);
+#else
+void system_print(bool visible, char* pFile, char* pMsg, ...);
+#endif
+
+#if defined(NONMATCHING) || defined(COMPAT_ANY)
+void system_halt(const char* pFile, int line, const char* pMsg, ...);
+#else
+void system_halt(char* pFile, int line, char* pMsg, ...);
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+#endif
