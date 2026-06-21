dc176ceb60ed67ad5a78a25afe96b9ef3253ffe0
vulcanbleh <148649099+vulcanbleh@users.noreply.github.com>
2026-06-20 21:14:18 -0400

    usb matched
    impls mainly from ogws and smg

--- include/RevoSDK/OS/OSCache.h
+++ include/RevoSDK/OS/OSCache.h
@@ -1,5 +1,5 @@
-#ifndef _DOLPHIN_OS_OSCACHE_H
-#define _DOLPHIN_OS_OSCACHE_H
+#ifndef _REVOSDK_OS_OSCACHE_H
+#define _REVOSDK_OS_OSCACHE_H
 
 #include "RevoSDK/OS/OSThread.h"
 #include "RevoSDK/OS/OSUtil.h"
@@ -14,7 +14,7 @@ extern "C" {
 ///////// CACHE FUNCTIONS ////////
 // Data cache functions.
 void DCInvalidateRange(void* addr, u32 numBytes);
-void DCFlushRange(void* addr, u32 numBytes);
+void DCFlushRange(const void* addr, u32 numBytes);
 void DCStoreRange(void* addr, u32 numBytes);
 void DCFlushRangeNoSync(void* addr, u32 numBytes);
 void DCStoreRangeNoSync(void* addr, u32 numBytes);
@@ -28,9 +28,11 @@ void ICEnable();
 // Locked cache functions.
 void LCEnable();
 void LCDisable();
+void LCLoadBlocks(void* destTag, const void* srcAddr, u32 numBlocks);
 void LCStoreBlocks(void* destAddr, void* srcTag, u32 numBlocks);
 u32 LCStoreData(void* destAddr, void* srcAddr, u32 numBytes);
 void LCQueueWait(u32 length);
+u32 LCQueueLength();
 
 // L2 cache functions.
 void L2GlobalInvalidate();
@@ -44,9 +46,8 @@ void ICFreeze();
 void ICUnfreeze();
 void ICBlockInvalidate(void* addr);
 
-void LCLoadBlocks(void* destTag, void* srcAddr, u32 numBlocks);
+
 u32 LCLoadData(void* destAddr, void* srcAddr, u32 numBytes);
-u32 LCQueueLength();
 void LCFlushQueue();
 
 void L2Enable();
